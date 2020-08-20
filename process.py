964559c3a9df818c190e3650ef38b24f9dd2b918
Donnie Bryson <donniebryson@gmail.com>
2020-08-20 07:13:00 -0400

    Update process.py

--- process.py
+++ process.py
@@ -5,19 +5,13 @@ from fbprophet import Prophet
 client = boto3.client('s3')
 obj = client.get_object(Bucket='donniebrysonplaybucket', Key='input/example_retail_sales.csv')
 df = pd.read_csv(io.BytesIO(obj['Body'].read()))
-#df = pd.read_csv('input.csv', sep='\t')
 
-df.head()
-# Python
 m = Prophet()
 m.fit(df)
-# Python
 future = m.make_future_dataframe(periods=365)
 future.tail()
-# Python
 forecast = m.predict(future)
 output = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
 output.to_csv('donnie.csv')
 s3 = boto3.resource('s3')
 s3.Object('donniebrysonplaybucket','output/donnie.csv').upload_file(Filename='donnie.csv')
-#
